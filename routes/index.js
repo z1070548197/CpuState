91288745fa06bc42df3eebcd817ef7df07f7f583
z1070548197 <50348211+z1070548197@users.noreply.github.com>
2022-05-06 23:21:14 +0800

    new

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,12 @@ const cpu = require('../module/cpu')
 router.get('/', async (ctx, next) => {
 
 })
-
+let Set_AUTOSTATE =async (e) => {
+  let infos = (await Cpu.find())[0]
+  let _id = { _id: infos._id.toString() }
+  infos = { FanAutoState: e }
+  Cpu.updateOne(_id, infos, () => { })
+}
 router.get('/getCpuTem', async (ctx, next) => {
   ctx.body = cpu.getCpuTem()
   ctx.status = 200
@@ -15,36 +20,33 @@ router.get('/FanOff', async (ctx, next) => {
   ctx.status = 200
 })
 router.get('/FanOn', async (ctx, next) => {
-  ctx.body = cpu.FanON() 
+  ctx.body = cpu.FanON()
   ctx.status = 200
 })
 router.get('/switchFan', async (ctx, next) => {
-  if(ctx.query.state=='true'){
+  Set_AUTOSTATE(false)
+  if (ctx.query.state == 'true') {
     ctx.body = cpu.FanON()
-    console.log('123')
-  }else{
+  } else {
     ctx.body = cpu.FanOff()
     console.log('312')
   }
-  
   ctx.status = 200
 })
 router.get('/SetFan', async (ctx, next) => {
   let num = ctx.query.num
   ctx.body = cpu.SetFan(num)
-  ctx.status = 200 
+  ctx.status = 200
 })
 router.get('/SetAutoFan', async (ctx, next) => {
-  let infos = (await Cpu.find())[0]
-  let _id={_id:infos._id.toString() }
-  infos = {  FanAutoState: ctx.query.state }
-  Cpu.updateOne(_id,infos,()=>{})
-  ctx.body =  cpu.autoFan(ctx.query.state)
-  ctx.status = 200  
+  Set_AUTOSTATE(ctx.query.state)
+  ctx.body = cpu.autoFan(ctx.query.state)
+  ctx.status = 200
 })
+
 router.get('/FanInfo', async (ctx, next) => {
-  let infos = (await Cpu.find())[0]._doc  
- 
+  let infos = (await Cpu.find())[0]._doc
+
   ctx.body = { ...cpu.FanInfo(), ...infos }
   ctx.status = 200
 })
