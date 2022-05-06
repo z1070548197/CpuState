d7b438ef65d65fbbb16421caef82c7c13a115d75
z1070548197 <50348211+z1070548197@users.noreply.github.com>
2022-05-06 21:13:46 +0800

    new

---
+++ models/Cpu.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+ 
+// 实例化数据模板
+const UserSchema = new Schema({
+  fanNum: {
+    type: Number,
+    default:50
+  },
+  info:{
+    type:Object
+  },
+  FanAutoState:{
+    type:Boolean
+  }
+});
+ 
+module.exports = cpu = mongoose.model('cpu', UserSchema);
--- module/cpu.js
+++ module/cpu.js
@@ -1,18 +1,31 @@
 const rpio = require('rpio') //进入GPIO node库 
 const fs = require('fs')
 const CON = require('../utils/config')
+const DB = require('../module/db.js'); //引入db
+const Cpu = require('../models/Cpu'); //引入
+const { info } = require('console');
 let Max = CON.Max; //频率最高值
 let Min = CON.Min;//频率最低值
 let autoInterval = null; //定时器存储
 let pwm_pin = CON.pwm_pin;  // 排针第12引脚
-let PublicNum=50 //风扇转数 100为慢速
+let PublicNum=50 //默认风扇转数 100为慢速
 rpio.open(pwm_pin, rpio.PWM); // 设置为PWM输出
 rpio.pwmSetClockDivider(8); // 时钟8分频 具体是多少 可能示波器显示的会比较准确，目前没发现有文档说明
 rpio.pwmSetRange(pwm_pin, Max); // 设置PWM发生器范围
 // rpio.msleep(6); // 延时6毫秒
 const TEMP_FILE = CON.TEMP_FILE //cpu温度路径
 let FanAutoState = false  //自动风扇状态
-exports.FanInfo = () => {
+let Stateinfo=async ()=>{
+    if((await Cpu.find()).length===0){
+        new Cpu().save()
+    }
+}
+Stateinfo()
+exports.FanInfo =async () => {
+   let infos= await Cpu.find()
+    if( infos.length!==0){
+        PublicNum=PublicNum[0].fanNum
+    }
     const info={
         CON,PublicNum,FanAutoState
     }
