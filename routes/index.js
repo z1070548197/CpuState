06ed48e2cd0c8600667f9de6c28903b8fe81a634
z1070548197 <50348211+z1070548197@users.noreply.github.com>
2022-05-06 22:25:41 +0800

    new

--- routes/index.js
+++ routes/index.js
@@ -6,34 +6,34 @@ router.get('/', async (ctx, next) => {
 })
 
 router.get('/getCpuTem', async (ctx, next) => {
-  ctx.body= cpu.getCpuTem()
-  ctx.status=200
+  ctx.body = cpu.getCpuTem()
+  ctx.status = 200
 })
 
 router.get('/FanOff', async (ctx, next) => {
-  ctx.body= cpu.FanOff()
-  ctx.status=200
+  ctx.body = cpu.FanOff()
+  ctx.status = 200
 })
 router.get('/FanOn', async (ctx, next) => {
-  ctx.body= cpu.FanON()
-  ctx.status=200
+  ctx.body = cpu.FanON()
+  ctx.status = 200
 })
 router.get('/SetFan', async (ctx, next) => {
-    let num=ctx.query.num
-  ctx.body= cpu.SetFan(num)
-  ctx.status=200
+  let num = ctx.query.num
+  ctx.body = cpu.SetFan(num)
+  ctx.status = 200
 })
 router.get('/SetAutoFan', async (ctx, next) => {
-  let infos= await Cpu.find()
-  infos={...infos,FanAutoState:ctx.query.state}
-  Cpu.save(infos)
-ctx.body= cpu.autoFan(ctx.query.state)
-ctx.status=200
+  let infos = (await Cpu.find())[0]
+  infos = { ...infos, FanAutoState: ctx.query.state }
+  Cpu.updateOne(infos)
+  ctx.body = cpu.autoFan(ctx.query.state)
+  ctx.status = 200
 })
 router.get('/FanInfo', async (ctx, next) => {
-  let infos= await Cpu.find()
-  PublicNum=infos[0].fanNum
-  ctx.body={...cpu.FanInfo(),PublicNum} 
-  ctx.status=200
-  })
+  let infos = await Cpu.find()
+  PublicNum = infos[0].fanNum
+  ctx.body = { ...cpu.FanInfo(), PublicNum }
+  ctx.status = 200
+})
 module.exports = router
